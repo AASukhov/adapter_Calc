b4576f62f4fcbbf6bcc1db8ab6a534c7927fe204
AASukhov <arkhonfills@list.ru>
2022-01-21 02:41:12 +0300

    Initial commit

---
+++ src/IntsCalculator.java
@@ -0,0 +1,34 @@
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        Calculator.Formula formula = target.newFormula();
+        formula.addOperand(arg0)
+               .addOperand(arg1)
+               .calculate(Calculator.Operation.SUM);
+        return (int) Math.round(formula.result);
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        Calculator.Formula formula = target.newFormula();
+        formula.addOperand(arg0)
+               .addOperand(arg1)
+               .calculate(Calculator.Operation.MULT);
+        return (int) Math.round(formula.result);
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        Calculator.Formula formula = target.newFormula();
+        formula.addOperand(a)
+               .addOperand(b)
+               .calculate(Calculator.Operation.POW);
+        return (int) Math.round(formula.result);
+    }
+}
